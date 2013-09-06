7d858ad6963ef0ecfe645f6e5681e34e5fbeed34
Alexander <alextyu@Macintosh-442.local>
2013-09-06 13:50:27 -0400

    Updating basic code skeleton

---
+++ ruby_check.rb
@@ -0,0 +1,32 @@
+require 'rtc'
+
+
+#QuickCheck for Ruby
+class ruber_checker
+
+typesig ("test_gen: (.?) -> (.?)") 
+#//* @params types: Array of types, rules: Bloc of constraints
+#    @return valid test set
+def test_gen (types, rules)
+	
+end
+
+
+
+
+
+
+
+
+
+#Dummy test case for quicker testing purposes; Also serves as default for improper calls
+def default_test_case (x , y)
+	x + y
+end
+
+end
+
+#Data structure for test results
+class test_set
+
+end
