d1341362c4b77a83a1d54bf5ad83b77fef33eb4d
Alexander <alextyu@Macintosh-442.local>
2013-09-06 14:04:57 -0400

    Continued experimentation with logic structure

--- ruby_check.rb
+++ ruby_check.rb
@@ -1,18 +1,21 @@
-require 'rtc'
+#require 'rtc'
 
 
 #QuickCheck for Ruby
 class ruber_checker
 
-typesig ("test_gen: (.?) -> (.?)") 
-#//* @params types: Array of types, rules: Bloc of constraints
-#    @return valid test set
+#typesig ("test_gen: (.?) -> (.?)") 
+# @params types: Array of types, rules: Bloc of constraints, times: number of tests
+# @return valid test set
 def test_gen (types, rules)
-	
+	case types.get_type#[0].get_type
+	when int
+		return rand(255)
 end
 
-
-
+# @params 
+# @return
+def parse_method()
 
 
 
