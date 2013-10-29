530350b3e38689dc697c0b5ae6aae53d9d4f77a3
Alexander <alextyu@Macintosh-442.local>
2013-10-29 15:02:24 -0400

    Preparing for major logic overhaul
    No method 'new' for numerics
    Requires casting when testing (i.e. Numeric, 3.25.check [3.25==Float])

--- test.rb
+++ test.rb
@@ -1,8 +1,5 @@
 require_relative "rbqc"
 
-
-
-
 #pass
 rqc = RQC.new(String, :upcase, true) { |x| x.downcase == $upcase_p0.downcase }
 p "Hello World!".upcase(rqc)
@@ -11,7 +8,15 @@ p "Hello World!".upcase(rqc)
 rqc1 = RQC.new(String, :reverse, true) {|x| x.reverse == $reverse_p0}
 p "Hello World!".reverse(rqc1)
 
-
 #fail
 rqc2 = RQC.new(String, :downcase, true) {|x| x.reverse == $downcase_p0.upcase}
 p "hi".downcase(rqc2)
+
+#pass
+rqc4 = RQC.new(Float, :floor, true) {|x| x <= $floor_p0}
+p 3.25.floor(rqc4)
+
+#pass
+rqc3 = RQC.new(Numeric, :coerce, true) {|x| x[0].class==x[1].class}
+rqc3.spec_gen([Numeric])
+p 5.coerce(rqc3,6.2)
