e7af91589af2418a14635230b60c23f77fd23922
Alexander <alextyu@anatase.home>
2013-12-21 19:04:14 -0500

    Saving idea template

--- rubyhack.rb
+++ rubyhack.rb
@@ -7,20 +7,50 @@
 #end
 
 def hi(x) #x can be Numeric or String
+	p "hi called"
 	x = x*5
 end
 
 
-obj = "hello"
+obj = BasicObject.new
 class << obj
-	for i in self.methods do
-		p i
-		begin 
-			undef i
-		rescue
-			p ""
-		end
-		#remove_method i
+	def method_missing(name, *args, &blk)
+		return name
 	end
 end
-p obj.methods
+
+
+#p obj+1
+
+def blank(x)
+	eval("
+	obj = BasicObject.new; 
+	class << obj; 
+		def method_missing(name, *args, &blk);
+			
+			#{x.object_id}.send(name,*args,&blk)
+		end;
+	end;
+	")
+end
+
+class RQC_blank < BasicObject
+	
+	def initialize(x)
+		@obj = x
+	end
+	
+	def p (str)
+		"".instance_eval("p \"#{name}\"")
+	end
+	
+	def method_missing(name, *args, &blk)
+		#p name
+		@obj.send(name, *args, &blk)
+	end
+	
+end
+
+y = RQC_blank.new(5)
+p y.methods
+hi(5)
